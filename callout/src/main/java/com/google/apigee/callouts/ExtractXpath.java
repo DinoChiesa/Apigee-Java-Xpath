49a7f047c9900c2e998d82c2959fce42b9f1d0e3
Dino Chiesa <dchiesa@google.com>
2022-01-05 10:05:46 -0800

    proper config for antrun plugin
    - specify goals for the clean phase
    - use target instead of tasks
    - also gformat some modules
    - and add copyright where missing

--- callout/src/main/java/com/google/apigee/callouts/ExtractXpath.java
+++ callout/src/main/java/com/google/apigee/callouts/ExtractXpath.java
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Google LLC
+// Copyright 2018-2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,124 +18,115 @@ package com.google.apigee.callouts;
 import com.apigee.flow.execution.ExecutionContext;
 import com.apigee.flow.execution.ExecutionResult;
 import com.apigee.flow.execution.spi.Execution;
-import com.apigee.flow.message.Message;
 import com.apigee.flow.message.MessageContext;
 import com.google.apigee.util.XPathEvaluator;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import javax.xml.xpath.XPathConstants;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Attr;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class ExtractXpath extends XpathCalloutBase implements Execution {
 
-    public ExtractXpath(Map properties) {
-        super(properties);
+  public ExtractXpath(Map properties) {
+    super(properties);
+  }
+
+  private XPathEvaluator getXpe(MessageContext msgCtxt) throws Exception {
+    XPathEvaluator xpe = new XPathEvaluator();
+    // register namespaces
+    for (Object prop : properties.keySet()) {
+      String key = (String) prop;
+      if (key.startsWith("xmlns:")) {
+        String value = resolvePropertyValue((String) properties.get(key), msgCtxt);
+        String[] parts = key.split(":");
+        xpe.registerNamespace(parts[1], value);
+      }
     }
+    return xpe;
+  }
 
-    private XPathEvaluator getXpe(MessageContext msgCtxt) throws Exception {
-        XPathEvaluator xpe = new XPathEvaluator();
-        // register namespaces
-        for (Object prop : properties.keySet()) {
-            String key = (String) prop;
-            if (key.startsWith("xmlns:")) {
-                String value =
-                    resolvePropertyValue((String) properties.get(key), msgCtxt);
-                String[] parts = key.split(":");
-                xpe.registerNamespace(parts[1], value);
-            }
-        }
-        return xpe;
+  private Map<String, String> getVarXpathPairs(MessageContext msgCtxt) throws Exception {
+    Map<String, String> xpaths = new HashMap<String, String>();
+    for (Object prop : properties.keySet()) {
+      String key = (String) prop;
+      if (key.startsWith("xpath:")) {
+        String value = resolvePropertyValue((String) properties.get(key), msgCtxt);
+
+        String[] parts = key.split(":");
+        xpaths.put(parts[1], value);
+      }
     }
+    return xpaths;
+  }
 
-    private Map<String,String> getVarXpathPairs(MessageContext msgCtxt) throws Exception {
-        Map<String,String> xpaths = new HashMap<String,String>();
-        for (Object prop : properties.keySet()) {
-            String key = (String) prop;
-            if (key.startsWith("xpath:")) {
-                String value =
-                    resolvePropertyValue((String) properties.get(key), msgCtxt);
-
-                String[] parts = key.split(":");
-                xpaths.put(parts[1], value);
-            }
-        }
-        return xpaths;
+  private void validate(NodeList nodes) throws IllegalStateException {
+    int length = nodes.getLength();
+    if (length != 1) {
+      throw new IllegalStateException(
+          "xpath does not resolve to one node. (length=" + length + ")");
     }
+  }
 
-
-    private void validate(NodeList nodes) throws IllegalStateException {
-        int length = nodes.getLength();
-        if (length != 1) {
-            throw new IllegalStateException("xpath does not resolve to one node. (length=" + length + ")");
-        }
+  private String extract(NodeList nodes) {
+    Node currentNode = nodes.item(0);
+    short nodeType = currentNode.getNodeType();
+    switch (nodeType) {
+      case Node.ATTRIBUTE_NODE:
+        return ((Attr) currentNode).getNodeValue();
+      case Node.ELEMENT_NODE:
+        return ((Element) currentNode).getTextContent();
+      case Node.TEXT_NODE:
+        return currentNode.getNodeValue();
     }
+    return "";
+  }
 
-    private String extract(NodeList nodes) {
-        Node currentNode = nodes.item(0);
-        short nodeType = currentNode.getNodeType();
-        switch (nodeType) {
-            case Node.ATTRIBUTE_NODE:
-                return ((Attr)currentNode).getNodeValue();
-            case Node.ELEMENT_NODE:
-                return ((Element)currentNode).getTextContent();
-            case Node.TEXT_NODE:
-                return currentNode.getNodeValue();
-        }
-        return "";
-    }
+  public ExecutionResult execute(final MessageContext msgCtxt, final ExecutionContext execContext) {
+    try {
+      // 0. get the source document
+      Document document = getDocument(msgCtxt);
 
-    public ExecutionResult execute (final MessageContext msgCtxt,
-                                    final ExecutionContext execContext) {
+      // 1. get XPath evaluator
+      XPathEvaluator xpe = getXpe(msgCtxt);
+
+      // 2. get all XPaths
+      Map<String, String> varXpathPairs = getVarXpathPairs(msgCtxt);
+
+      if (varXpathPairs.entrySet().size() < 1) {
+        throw new IllegalStateException("no xpaths provided");
+      }
+
+      // 3. iterate through each xpath, evaluating and setting var
+      for (Map.Entry<String, String> entry : varXpathPairs.entrySet()) {
+        String variableName = entry.getKey();
+        String xpath = entry.getValue();
+        NodeList nodes = (NodeList) xpe.evaluate(xpath, document, XPathConstants.NODESET);
         try {
-            // 0. get the source document
-            Document document = getDocument(msgCtxt);
-
-            // 1. get XPath evaluator
-            XPathEvaluator xpe = getXpe(msgCtxt);
-
-            // 2. get all XPaths
-            Map<String,String> varXpathPairs = getVarXpathPairs(msgCtxt);
-
-            if (varXpathPairs.entrySet().size() < 1) {
-                throw new IllegalStateException("no xpaths provided");
-            }
-
-            // 3. iterate through each xpath, evaluating and setting var
-            for (Map.Entry<String, String> entry : varXpathPairs.entrySet()) {
-                String variableName = entry.getKey();
-                String xpath = entry.getValue();
-                NodeList nodes = (NodeList)xpe.evaluate(xpath, document, XPathConstants.NODESET);
-                try {
-                    validate(nodes);
-                    msgCtxt.setVariable(variableName, extract(nodes));
-                }
-                catch (IllegalStateException exc1) {
-                    setExceptionVariables(exc1,msgCtxt);
-                }
-            }
-
-            return ExecutionResult.SUCCESS;
+          validate(nodes);
+          msgCtxt.setVariable(variableName, extract(nodes));
+        } catch (IllegalStateException exc1) {
+          setExceptionVariables(exc1, msgCtxt);
         }
-        catch (javax.xml.xpath.XPathExpressionException texc1){
-            setExceptionVariables(texc1,msgCtxt);
-            return ExecutionResult.ABORT;
-        }
-        catch (IllegalStateException exc1) {
-            setExceptionVariables(exc1,msgCtxt);
-            return ExecutionResult.ABORT;
-        }
-        catch (Exception e) {
-            if (getDebug()) {
-                System.out.println(getStackTrace(e));
-            }
-            setExceptionVariables(e,msgCtxt);
-            msgCtxt.setVariable(varName("stacktrace"), getStackTrace(e));
-            return ExecutionResult.ABORT;
-        }
+      }
+
+      return ExecutionResult.SUCCESS;
+    } catch (javax.xml.xpath.XPathExpressionException texc1) {
+      setExceptionVariables(texc1, msgCtxt);
+      return ExecutionResult.ABORT;
+    } catch (IllegalStateException exc1) {
+      setExceptionVariables(exc1, msgCtxt);
+      return ExecutionResult.ABORT;
+    } catch (Exception e) {
+      if (getDebug()) {
+        System.out.println(getStackTrace(e));
+      }
+      setExceptionVariables(e, msgCtxt);
+      msgCtxt.setVariable(varName("stacktrace"), getStackTrace(e));
+      return ExecutionResult.ABORT;
     }
-
+  }
 }
--- callout/src/main/java/com/google/apigee/callouts/XpathCalloutBase.java
+++ callout/src/main/java/com/google/apigee/callouts/XpathCalloutBase.java
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Google LLC
+// Copyright 2018-2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,119 +25,127 @@ import java.util.regex.Pattern;
 import org.w3c.dom.Document;
 
 public abstract class XpathCalloutBase {
-    private final static String _varprefix= "xpath_";
-    protected Map properties; // read-only
-    private static final String variableReferencePatternString = "(.*?)\\{([^\\{\\} ]+?)\\}(.*?)";
-    private static final Pattern variableReferencePattern = Pattern.compile(variableReferencePatternString);
+  private static final String _varprefix = "xpath_";
+  protected Map properties; // read-only
+  private static final String variableReferencePatternString = "(.*?)\\{([^\\{\\} ]+?)\\}(.*?)";
+  private static final Pattern variableReferencePattern =
+      Pattern.compile(variableReferencePatternString);
 
-    public XpathCalloutBase(Map properties) {
-        this.properties = properties;
+  public XpathCalloutBase(Map properties) {
+    this.properties = properties;
+  }
+
+  static String varName(String s) {
+    return _varprefix + s;
+  }
+
+  protected Document getDocument(MessageContext msgCtxt) throws Exception {
+    String source = getSimpleOptionalProperty("source", msgCtxt);
+    if (source == null) {
+      return XmlUtils.parseXml(msgCtxt.getMessage().getContentAsStream());
     }
-
-    static String varName(String s) { return _varprefix + s; }
-
-    protected Document getDocument(MessageContext msgCtxt) throws Exception {
-        String source = getSimpleOptionalProperty("source", msgCtxt);
-        if (source == null) {
-            return XmlUtils.parseXml(msgCtxt.getMessage().getContentAsStream());
-        }
-        String text = (String) msgCtxt.getVariable(source);
-        if (text == null) {
-            throw new IllegalStateException("source variable resolves to null");
-        }
-        return XmlUtils.parseXml(text);
+    String text = (String) msgCtxt.getVariable(source);
+    if (text == null) {
+      throw new IllegalStateException("source variable resolves to null");
     }
+    return XmlUtils.parseXml(text);
+  }
 
-    protected static String getStackTrace(Throwable e) {
-        StringWriter sw = new StringWriter();
-        e.printStackTrace(new PrintWriter(sw));
-        return sw.toString();
+  protected static String getStackTrace(Throwable e) {
+    StringWriter sw = new StringWriter();
+    e.printStackTrace(new PrintWriter(sw));
+    return sw.toString();
+  }
+
+  // protected boolean getPretty(MessageContext msgCtxt) throws Exception {
+  //     String pretty = getSimpleOptionalProperty("pretty", msgCtxt);
+  //     if (pretty == null) return false;
+  //     pretty = pretty.toLowerCase();
+  //     return pretty.equals("true");
+  // }
+
+  protected boolean getDebug() {
+    String value = (String) this.properties.get("debug");
+    if (value == null) return false;
+    if (value.trim().toLowerCase().equals("true")) return true;
+    return false;
+  }
+
+  // protected String getOutputVar(MessageContext msgCtxt) throws Exception {
+  //     String dest = getSimpleOptionalProperty("output-variable", msgCtxt);
+  //     if (dest == null) {
+  //         return "message.content";
+  //     }
+  //     return dest;
+  // }
+
+  protected String getSimpleOptionalProperty(String propName, MessageContext msgCtxt)
+      throws Exception {
+    String value = (String) this.properties.get(propName);
+    if (value == null) {
+      return null;
     }
-
-    // protected boolean getPretty(MessageContext msgCtxt) throws Exception {
-    //     String pretty = getSimpleOptionalProperty("pretty", msgCtxt);
-    //     if (pretty == null) return false;
-    //     pretty = pretty.toLowerCase();
-    //     return pretty.equals("true");
-    // }
-
-    protected boolean getDebug() {
-        String value = (String) this.properties.get("debug");
-        if (value == null) return false;
-        if (value.trim().toLowerCase().equals("true")) return true;
-        return false;
+    value = value.trim();
+    if (value.equals("")) {
+      return null;
     }
-
-    // protected String getOutputVar(MessageContext msgCtxt) throws Exception {
-    //     String dest = getSimpleOptionalProperty("output-variable", msgCtxt);
-    //     if (dest == null) {
-    //         return "message.content";
-    //     }
-    //     return dest;
-    // }
-
-    protected String getSimpleOptionalProperty(String propName, MessageContext msgCtxt) throws Exception {
-        String value = (String) this.properties.get(propName);
-        if (value == null) { return null; }
-        value = value.trim();
-        if (value.equals("")) { return null; }
-        value = resolvePropertyValue(value, msgCtxt);
-        if (value == null || value.equals("")) { return null; }
-        return value;
+    value = resolvePropertyValue(value, msgCtxt);
+    if (value == null || value.equals("")) {
+      return null;
     }
+    return value;
+  }
 
-    protected String getSimpleRequiredProperty(String propName, MessageContext msgCtxt) throws Exception {
-        String value = (String) this.properties.get(propName);
-        if (value == null) {
-            throw new IllegalStateException(propName + " resolves to an empty string");
-        }
-        value = value.trim();
-        if (value.equals("")) {
-            throw new IllegalStateException(propName + " resolves to an empty string");
-        }
-        value = resolvePropertyValue(value, msgCtxt);
-        if (value == null || value.equals("")) {
-            throw new IllegalStateException(propName + " resolves to an empty string");
-        }
-        return value;
+  protected String getSimpleRequiredProperty(String propName, MessageContext msgCtxt)
+      throws Exception {
+    String value = (String) this.properties.get(propName);
+    if (value == null) {
+      throw new IllegalStateException(propName + " resolves to an empty string");
     }
-
-    // If the value of a property contains any pairs of curlies,
-    // eg, {apiproxy.name}, then "resolve" the value by de-referencing
-    // the context variables whose names appear between curlies.
-    protected String resolvePropertyValue(String spec, MessageContext msgCtxt) {
-        Matcher matcher = variableReferencePattern.matcher(spec);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, "");
-            sb.append(matcher.group(1));
-            Object v = msgCtxt.getVariable(matcher.group(2));
-            if (v != null){
-                sb.append((String) v );
-            }
-            sb.append(matcher.group(3));
-        }
-        matcher.appendTail(sb);
-        return sb.toString();
+    value = value.trim();
+    if (value.equals("")) {
+      throw new IllegalStateException(propName + " resolves to an empty string");
     }
-
-    private static final String qualifiedClassNameRegex =
-        "(?:[a-zA-Z_$][a-zA-Z0-9_$]*\\.)+[a-zA-Z_$][a-zA-Z0-9_$]*";
-    private static final String errorSuffixRegex =
-        ": (.+)";
-    private static final Pattern errorStringPattern =
-        Pattern.compile("^.*"+ qualifiedClassNameRegex + errorSuffixRegex + "$");
-
-    protected void setExceptionVariables(Exception exc1, MessageContext msgCtxt) {
-        String error = exc1.toString();
-        msgCtxt.setVariable(varName("exception"), error);
-        Matcher matcher = errorStringPattern.matcher(error);
-        if (matcher.find()) {
-            msgCtxt.setVariable(varName("error"), matcher.group(1));
-        }
-        else {
-            msgCtxt.setVariable(varName("error"), error);
-        }
+    value = resolvePropertyValue(value, msgCtxt);
+    if (value == null || value.equals("")) {
+      throw new IllegalStateException(propName + " resolves to an empty string");
     }
+    return value;
+  }
 
+  // If the value of a property contains any pairs of curlies,
+  // eg, {apiproxy.name}, then "resolve" the value by de-referencing
+  // the context variables whose names appear between curlies.
+  protected String resolvePropertyValue(String spec, MessageContext msgCtxt) {
+    Matcher matcher = variableReferencePattern.matcher(spec);
+    StringBuffer sb = new StringBuffer();
+    while (matcher.find()) {
+      matcher.appendReplacement(sb, "");
+      sb.append(matcher.group(1));
+      Object v = msgCtxt.getVariable(matcher.group(2));
+      if (v != null) {
+        sb.append((String) v);
+      }
+      sb.append(matcher.group(3));
+    }
+    matcher.appendTail(sb);
+    return sb.toString();
+  }
+
+  private static final String qualifiedClassNameRegex =
+      "(?:[a-zA-Z_$][a-zA-Z0-9_$]*\\.)+[a-zA-Z_$][a-zA-Z0-9_$]*";
+  private static final String errorSuffixRegex = ": (.+)";
+  private static final Pattern errorStringPattern =
+      Pattern.compile("^.*" + qualifiedClassNameRegex + errorSuffixRegex + "$");
+
+  protected void setExceptionVariables(Exception exc1, MessageContext msgCtxt) {
+    String error = exc1.toString();
+    msgCtxt.setVariable(varName("exception"), error);
+    Matcher matcher = errorStringPattern.matcher(error);
+    if (matcher.find()) {
+      msgCtxt.setVariable(varName("error"), matcher.group(1));
+    } else {
+      msgCtxt.setVariable(varName("error"), error);
+    }
+  }
 }
